070673d3bcad22b60fd7caa908c8ebe46a941e99
Rafał Selewońko <rafal@selewonko.com>
2016-05-24 10:31:07 +0200

    Update

--- mcp3008.py
+++ mcp3008.py
@@ -14,35 +14,35 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
     """
     Read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
     """
-    if ((adcnum > 7) or (adcnum < 0)):
-        return -1
-    GPIO.output(cspin, True)
+    assert 0 <= adcnum < 8
 
-    GPIO.output(clockpin, False)  # start clock low
-    GPIO.output(cspin, False)     # bring CS low
+    GPIO.output(cspin, GPIO.HIGH)
+    GPIO.output(clockpin, GPIO.LOW)  # start clock low
+    GPIO.output(cspin, GPIO.HIGH)     # bring CS low
 
     commandout = adcnum
     commandout |= 0x18  # start bit + single-ended bit
     commandout <<= 3    # we only need to send 5 bits here
+
     for i in range(5):
         if (commandout & 0x80):
-            GPIO.output(mosipin, True)
+            GPIO.output(mosipin, GPIO.HIGH)
         else:
-            GPIO.output(mosipin, False)
+            GPIO.output(mosipin, GPIO.LOW)
         commandout <<= 1
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
 
     adcout = 0
     # read in one empty bit, one null bit and 10 ADC bits
     for i in range(12):
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
+        GPIO.output(clockpin, GPIO.HIGH)
+        GPIO.output(clockpin, GPIO.LOW)
         adcout <<= 1
         if GPIO.input(misopin):
             adcout |= 0x1
 
-    GPIO.output(cspin, True)
+    GPIO.output(cspin, GPIO.HIGH)
 
     adcout >>= 1       # first bit is 'null' so drop it
     return adcout
