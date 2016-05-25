2324ce115a73c362d0b5a5e72e5cc91b90c25554
Rafał Selewońko <rafal@selewonko.com>
2016-05-25 13:56:05 +0200

    Wysyłanie info na slacka, że ktoś otwarł

--- domofon/lokaj.py
+++ domofon/lokaj.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 from domofon.klawisz import Klawisz
 
@@ -6,6 +8,14 @@ DEBUG = True
 CHANNEL = 14
 SECONDS = 5
 KLAWISZ = Klawisz(CHANNEL)
+SLACK_HOOK_URL = 'https://hooks.slack.com/services/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'
+SLACK_HOOK_HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+SLACK_HOOK_DATA = json.dumps({
+    "channel": "#office",
+    "username": "Odźwierny",
+    "text": "Otwarłem!",
+    "icon_emoji": ":door:",
+})
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -19,6 +29,7 @@ def lokaj():
 @app.route("/otwórz/")
 def otwórz():
     KLAWISZ.otwórz(SECONDS)
+    requests.post(SLACK_HOOK_URL, data=SLACK_HOOK_DATA, headers=SLACK_HOOK_HEADERS)
     return "Otwarte"
 
 
