05e74da663f66e2511528e00b36a5c078bf3dd05
Rafał Selewońko <rafal@selewonko.com>
2016-05-25 14:59:12 +0200

    Wysyłanie nazwy hosta w wiadomości do slacka

--- domofon/lokaj.py
+++ domofon/lokaj.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
+import subprocess
+import re
+import os
 
-from domofon.klawisz import Klawisz
+# from domofon.klawisz import Klawisz
 
 CHANNEL = 14
 SECONDS = 5
-KLAWISZ = Klawisz(CHANNEL)
+# KLAWISZ = Klawisz(CHANNEL)
 SLACK_HOOK_URL = 'https://hooks.slack.com/services/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'
 SLACK_HOOK_HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-SLACK_HOOK_DATA = json.dumps({
+SLACK_HOOK_DATA = {
     "channel": "#office",
     "username": "Odźwierny",
-    "text": "Otwarłem!",
+    "text": "Otwarłem ({})!",
     "icon_emoji": ":door:",
-})
+}
 
+DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+def get_name(ip):
+    p = subprocess.Popen(('nmap', '-sP', ip), stdout=subprocess.PIPE,
+                         cwd=os.path.dirname(__file__))
+    string = p.communicate()[0].decode().strip()
+    pattern = r"Nmap scan report for (.*) \({}\)".format(ip)
+    search = re.search(pattern, string)
+    if search and search.groups():
+        name = search.groups()[0]
+        if name.endswith('.lan'):
+            name = name[0:-4]
+        return name
 
 
 @app.route("/")
@@ -27,8 +43,11 @@ def lokaj():
 
 @app.route("/otwórz/")
 def otwórz():
+    data = SLACK_HOOK_DATA.copy()
+    data['text'] = data['text'].format(get_name(request.remote_addr))
     KLAWISZ.otwórz(SECONDS)
-    requests.post(SLACK_HOOK_URL, data=SLACK_HOOK_DATA, headers=SLACK_HOOK_HEADERS)
+    requests.post(SLACK_HOOK_URL, data=json.dumps(data), headers=SLACK_HOOK_HEADERS)
+    print(data['text'])
     return "Otwarte"
 
 
